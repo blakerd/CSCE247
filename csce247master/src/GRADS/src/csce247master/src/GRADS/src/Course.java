e41074e809c0bca02608a54ba90c9fd99f516cc7
blakerd <45436210+blakerd@users.noreply.github.com>
2019-05-04 18:10:37 -0400

    Add files via upload
    Added an unfinished GRADSDataBase

---
+++ csce247master/src/GRADS/src/csce247master/src/GRADS/src/Course.java
@@ -0,0 +1,64 @@
+package GRADS.src;
+
+import GRADS.src.GRADSResources.BuildingFile;
+
+public class Course {
+
+    private String name;
+    private String courseID;
+    private String numCredits;
+
+    public Course(String name, String courseID, String numCredits) {
+        setName(name);
+        setCourseID(courseID);
+        setNumCredits(numCredits);
+    }
+
+    public Course() {
+        this.name = "noName";
+        this.courseID = "NoCourseID";
+        this.numCredits = "noCredits";
+    }
+
+    public Course(BuildingFile bf) {
+         setName(bf.getData()[0]);
+         setCourseID(bf.getData()[1]);
+         setNumCredits(bf.getData()[2]);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && name.length() != 0)
+            this.name = name;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        if (courseID.length() > 0)
+            this.courseID = courseID;
+    }
+
+    public String getNumCredits() {
+        return numCredits;
+    }
+
+    public void setNumCredits(String numCredits) {
+        if (numCredits != null && numCredits.length() > 0)
+            this.numCredits = numCredits;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", courseID=" + courseID +
+                ", numCredits='" + numCredits + '\'' +
+                '}';
+    }
+}
