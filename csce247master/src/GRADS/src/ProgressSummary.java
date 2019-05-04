405caa6ce5cf470a5007e1052ae385a29ec9aea0
blakerd <blakerd@email.sc.edu>
2019-05-04 18:50:21 -0400

    Update ProgressSummary.java

--- csce247master/src/GRADS/src/ProgressSummary.java
+++ csce247master/src/GRADS/src/ProgressSummary.java
@@ -1,5 +1,68 @@
 package GRADS.src;
 
+import GRADS.src.GRADSResources.EnumHandler.DeptMajorEnum;
+
 public class ProgressSummary {
 
+	private Student student;
+	private DeptMajorEnum major;
+	private double gradPercent;
+	private double majorGPA;
+	private double overallGPA;
+	private TermBegan termBegan;
+	private Advisor advisor;
+	private ReqCheckResults reqChecklist;
+	
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public DeptMajorEnum getMajor() {
+		return major;
+	}
+	public void setMajor(DeptMajorEnum major) {
+		this.major = major;
+	}
+	public double getGradPercent() {
+		return gradPercent;
+	}
+	public void setGradPercent(double gradPercent) {
+		this.gradPercent = gradPercent;
+	}
+	public double getMajorGPA() {
+		return majorGPA;
+	}
+	public void setMajorGPA(double majorGPA) {
+		this.majorGPA = majorGPA;
+	}
+	public double getOverallGPA() {
+		return overallGPA;
+	}
+	public void setOverallGPA(double overallGPA) {
+		this.overallGPA = overallGPA;
+	}
+	public TermBegan getTermBegan() {
+		return termBegan;
+	}
+	public void setTermBegan(TermBegan termBegan) {
+		this.termBegan = termBegan;
+	}
+	public Advisor getAdvisor() {
+		return advisor;
+	}
+	public void setAdvisor(Advisor advisor) {
+		this.advisor = advisor;
+	}
+	public ReqCheckResults getReqChecklist() {
+		return reqChecklist;
+	}
+	public void setReqChecklist(ReqCheckResults reqChecklist) {
+		this.reqChecklist = reqChecklist;
+	}
+	
+	public String toString() {
+		return student.getStudentID() + " " + student.getFirstName() + " " + student.getLastName() + " " + major + " " + gradPercent + " " + majorGPA + " " + overallGPA + " " + termBegan.getYear() + " " + termBegan.getSemester() + " " + advisor.getFirstName() + " " + advisor.getLastName() + " " + reqChecklist.getResults();
+    }
 }
