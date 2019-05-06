5aa61dcf38a04909c78bcc0de3b7a9d7effad646
blakerd <blakerd@email.sc.edu>
2019-05-06 13:48:59 -0400

    Documentation
    Added JAVADOC commenting to advisor and check

--- csce247master/src/GRADS/src/Advisor.java
+++ csce247master/src/GRADS/src/Advisor.java
@@ -3,11 +3,18 @@ package GRADS.src;
 import GRADS.src.GRADSResources.EnumHandler.*;
 
 public class Advisor extends User {
-
+    /**
+    */
     public Advisor(){
         super();
     }
-
+    /**
+    * This is a constructor that sets the given attributes of advisor.
+    * @param networkID is the advisors network ID
+    * @param firstName is the advisors first name
+    * @param lastName is the advisors last name
+    * @param program is the advisors department 
+    */
     public Advisor(String networkID, String firstName, String lastName, DeptMajorEnum program){
         super(networkID,firstName,lastName,UserEnum.GRADUATE_PROGRAM_COORDINATOR,program);
     }
--- csce247master/src/GRADS/src/Check.java
+++ csce247master/src/GRADS/src/Check.java
@@ -5,32 +5,49 @@ public class Check {
 	private String name;
 	private Details details;
 	private boolean passed;
-	
+	/**
+	* Constructor that sets all the given parameters for Check
+	* @param name is the name of the class
+	* @param details holds the details of the given class
+	* @param passed is a boolean value to represent whether the class was passed
+	*/
 	public Check(String name, Details details, boolean passed) {
 		setName(name);
 		setDetails(details);
 		setPassed(passed);
 	}
-	
+	/**
+	* Default constructor for Check
+	*/
 	public Check() {
 		this.name = "No Name";
 		this.details = new Details();
 		this.passed = false;
 	}
-
+	/**
+	* Method that returns the name of the calling instance
+	*/
 	public String getName() {
 		return name;
 	}
-
+	/**
+	* Method that sets the name of the calling instance to name
+	* @param name is the name of the given class
+	*/
 	public void setName(String name) {
         if (name != null && name.length() != 0)
             this.name = name;
     }
-
+		/**
+		* Method to return the details of the calling instance
+		*/
 	public Details getDetails() {
 		return details;
 	}
-
+	/**
+	* Method that sets the details of the given class to details
+	* @param details is the details of the given class
+	*/
 	public void setDetails(Details details) {
 		this.details = details;
 	}
@@ -42,5 +59,5 @@ public class Check {
 	public void setPassed(boolean passed) {
 		this.passed = passed;
 	}
-	
+
 }
