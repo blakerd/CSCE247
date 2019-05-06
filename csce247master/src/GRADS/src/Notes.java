50241036341fd53a1dc028e6303ff4f062d1b5fe
blakerd <blakerd@email.sc.edu>
2019-05-06 14:50:33 -0400

    Update Notes.java

--- csce247master/src/GRADS/src/Notes.java
+++ csce247master/src/GRADS/src/Notes.java
@@ -4,23 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Notes {
-
+  /**
+  *  This will be a list to hold all the notes for a given student
+  */
     private ArrayList<String> notes;
-
+    /**
+    * The default constructor for notes that initializes a new arraylist
+    */
     public Notes() {
         notes = new ArrayList<String>();
     }
-
+    /**
+    * Notes constructor that will set the list
+    * @param notes is a list with the notes for a given student
+    */
     public Notes(List<String> notes) {
         this.notes.addAll(notes);
         //TODO: LOG TO SYSLOG
     }
-
+    /**
+    * This will allow a note to be added to the notes list
+    * @param note is a note to be added to the notes list
+    */
     public void addNote(String note) {
         this.notes.add(note);
         //TODO: LOG TO SYSLOG
     }
-
+    /**
+    * This will allow all notes for a given student to be added to the notes list
+    * @param notes is a list with all the given students notes
+    */
     public void addNotes(List<String> notes) {
         this.notes.addAll(notes);
     }
@@ -32,7 +45,10 @@ public class Notes {
         //notes.clear();
         this.notes = new ArrayList<String>();
     }
-
+    /**
+    * This will allow the removal of notes from the notes list
+    * @param note is the note to be removed
+    */
     public void removeNote(String note) {
         for (int i = 0; i < notes.size(); i++) {
             if (note.equalsIgnoreCase(notes.get(i))) {
@@ -41,7 +57,10 @@ public class Notes {
             }
         }
     }
-    
+    /**
+  	* This is a method ised to concatenate all of the data of the courses taken list class
+    * @return the concatenated string
+  	*/
     public String toString() {
     	String returnSt = "";
     	for(String a : notes) {
